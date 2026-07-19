631bd7175cec1aff9492dc03c598175e0ab4969d
AlanL1214 <alaniscool070@gmail.com>
2026-07-19 21:06:30 +0800

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
+$msg = $_POST['message'] ?? '';
+
+$entry = "[" . date('Y-m-d H:i:s') . "] Name: $name | Email: $email | Message: $msg\n";
+
+file_put_contents("messages.txt", $entry, FILE_APPEND);
+
+header("Location: login.php");
+exit();
+?>
